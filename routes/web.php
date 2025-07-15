bbaae091dccfdabd16bb10f1d3c9836c85e01986
rafi021 <sitc.mahmud@gmail.com>
2025-07-15 20:27:41 +0600

    the concept of routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* 1. Create route defination
+2. Create a view file
+*/
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact-us', function () {
+    return view('contact');
+});
+Route::get('/services', function () {
+    return view('service');
+});
