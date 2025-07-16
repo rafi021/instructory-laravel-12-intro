0fdf270e9c6319fc7ece2d7a7bf77a1dceb82eb5
rafi021 <sitc.mahmud@gmail.com>
2025-07-16 14:57:39 +0600

    Constracting possble route parameters with validation

---
+++ app/CategroyEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+enum CategroyEnum: string
+{
+    case electronics = 'electronics';
+    case tv = 'tv';
+    case mobile = 'mobile';
+}
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\CategroyEnum;
+
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::view('/', 'home')->name('home');
+
 Route::get('/about-us-opurseldasdasda', function () {
     return view('about');
 })->name('about');
 Route::get('/contact-us', function () {
     return view('contact');
 })->name('contact');
-Route::get('/services', function () {
+
+Route::get('/services/{service_id}/{service_name?}', function ($service_id, ?string $service_name = null) {
+    dd($service_id, $service_name);
     return view('service');
 })->name('service');
+
+
+Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    return [
+        'id' => $id,
+        'name' => $name
+    ];
+})->where(['id' =>  '[0-9]+', 'name' => '[A-Za-z]+']);
+
+Route::get('/category/{category}', function (string $category) {
+    return $category;
+})->whereIn('category', CategroyEnum::cases());
