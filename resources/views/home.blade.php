d64dccb313fb29d195a8ce9e6f0d402cdaa3620c
rafi021 <sitc.mahmud@gmail.com>
2025-07-19 18:44:30 +0600

    partial rending

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('welcome')
 @section('main-content')
-    <h1>Home Page</h1>
-    {{ $data['name'] }}
+    <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+            <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                @forelse ($products as $key => $product)
+                    @include('partial.product-card', ['product' => $product])
+                @empty
+                    <li class="ps-3">No Products</li>
+                @endforelse
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,79 @@ Route::get('/', function () {
         'address' => 'Dhaka',
         'phone' => '123123123'
     ];
-    return view('home', compact('data'));
+
+    /*Create a associative array of products */
+    $products = [
+        [
+            "id" => 91,
+            "title" => "Efficient 2-Slice Toaster",
+            "slug" => "efficient-2-slice-toaster",
+            "price" => 48,
+            "description" => "Enhance your morning routine with our sleek 2-slice toaster, featuring adjustable browning controls and a removable crumb tray for easy cleaning. This compact and stylish appliance is perfect for any kitchen, ensuring your toast is always golden brown and delicious.",
+            "category" => [
+                "id" => 18,
+                "name" => "Electronics",
+                "slug" => "electronics",
+                "image" => "https://i.imgur.com/ZANVnHE.jpeg",
+                "creationAt" => "2025-07-19T09:03:44.000Z",
+                "updatedAt" => "2025-07-19T09:03:44.000Z"
+            ],
+            "images" => [
+                "https://i.imgur.com/keVCVIa.jpeg",
+                "https://i.imgur.com/afHY7v2.jpeg",
+                "https://i.imgur.com/yAOihUe.jpeg"
+            ],
+            "creationAt" => "2025-07-19T09:03:49.000Z",
+            "updatedAt" => "2025-07-19T09:03:49.000Z"
+        ],
+        [
+            "id" => 92,
+            "title" => "Sleek Comfort-Fit Over-Ear Headphones",
+            "slug" => "sleek-comfort-fit-over-ear-headphones",
+            "price" => 28,
+            "description" => "Experience superior sound quality with our Sleek Comfort-Fit Over-Ear Headphones, designed for prolonged use with cushioned ear cups and an adjustable, padded headband. Ideal for immersive listening, whether you're at home, in the office, or on the move. Their durable construction and timeless design provide both aesthetically pleasing looks and long-lasting performance.",
+            "category" => [
+                "id" => 18,
+                "name" => "Electronics",
+                "slug" => "electronics",
+                "image" => "https://i.imgur.com/ZANVnHE.jpeg",
+                "creationAt" => "2025-07-19T09:03:44.000Z",
+                "updatedAt" => "2025-07-19T09:03:44.000Z"
+            ],
+            "images" => [
+                "https://i.imgur.com/SolkFEB.jpeg",
+                "https://i.imgur.com/KIGW49u.jpeg",
+                "https://i.imgur.com/mWwek7p.jpeg"
+            ],
+            "creationAt" => "2025-07-19T09:03:49.000Z",
+            "updatedAt" => "2025-07-19T09:03:49.000Z"
+        ],
+        [
+            "id" => 93,
+            "title" => "Sleek Wireless Computer Mouse",
+            "slug" => "sleek-wireless-computer-mouse",
+            "price" => 100,
+            "description" => "Experience smooth and precise navigation with this modern wireless mouse, featuring a glossy finish and a comfortable ergonomic design. Its responsive tracking and easy-to-use interface make it the perfect accessory for any desktop or laptop setup. The stylish blue hue adds a splash of color to your workspace, while its compact size ensures it fits neatly in your bag for on-the-go productivity.",
+            "category" => [
+                "id" => 18,
+                "name" => "Electronics",
+                "slug" => "electronics",
+                "image" => "https://i.imgur.com/ZANVnHE.jpeg",
+                "creationAt" => "2025-07-19T09:03:44.000Z",
+                "updatedAt" => "2025-07-19T09:03:44.000Z"
+            ],
+            "images" => [
+                "https://i.imgur.com/w3Y8NwQ.jpeg"
+            ],
+            "creationAt" => "2025-07-19T09:03:49.000Z",
+            "updatedAt" => "2025-07-19T09:16:06.000Z"
+        ]
+    ];
+
+
+
+
+    return view('home', compact('data', 'products'));
 })->name('home');
 
 
@@ -34,7 +106,6 @@ Route::get('/services', function () {
     $isServices = false;
 
 
-    
 
 
     return view('service', compact('services', 'isServices'));
