9c6d1948905b14bd5944b1aee294007618a3887f
rafi021 <sitc.mahmud@gmail.com>
2025-07-21 18:44:16 +0600

    return file download

--- routes/web.php
+++ routes/web.php
@@ -28,16 +28,48 @@ Route::get('/', function (Request $request) {
         "Age" => 37,
     ];
 
+    $products = [
+        [
+            'title' => 'Laptop',
+            'category' => [
+                'name' => 'Electronics',
+            ],
+            'price' => 1200,
+            'description' => 'A high-performance laptop for professionals.',
+            'images' => ['https://example.com/laptop.jpg']
+        ],
+        [
+            'title' => 'Smartphone',
+            'category' => [
+                'name' => 'Electronics',
+            ],
+            'price' => 800,
+            'description' => 'A latest model smartphone with advanced features.',
+            'images' => ['https://example.com/laptop.jpg']
+        ],
+        [
+            'title' => 'Headphones',
+            'category' => [
+                'name' => 'Electronics',
+            ],
+            'price' => 150,
+            'description' => 'Noise-cancelling headphones for an immersive experience.',
+            'images' => ['https://example.com/laptop.jpg']
+        ]
+    ];
+
     // return $data;
-    return response($data)
-        ->header('Content-Type', 'application/json')
-        ->header('shop_id', 812312)
-        ->header('shop_name', 'Mahmud Ibrahim')
-        ->cookie('MyIDCard', '123343453465465', 3600);
-
-
-
-    return view('home', compact('data', 'products'));
+    // return response($data)
+    //     ->header('Content-Type', 'application/json')
+    //     ->header('shop_id', 812312)
+    //     ->header('shop_name', 'Mahmud Ibrahim')
+    //     ->cookie('MyIDCard', '123343453465465', 3600);
+    $isMissing = true;
+    if (!$isMissing) {
+        return view('home', compact('data', 'products'));
+    } else {
+        return redirect()->route('about');
+    }
 })->name('home');
 
 
@@ -93,3 +125,11 @@ Route::get('/user/{id}/{name}', function (string $id, string $name) {
 Route::get('/category/{category}', function (string $category) {
     return $category;
 })->whereIn('category', CategroyEnum::cases());
+
+
+Route::get('download', function () {
+    return response()->download(public_path('laravel_book.pdf'), 'Laravel New Book.pdf', [
+        'Content-Type' => 'application/pdf',
+        'Content-Disposition' => 'attachment; filename="Laravel New Book.pdf"'
+    ])->setStatusCode(200, 'Download Successful');
+});
