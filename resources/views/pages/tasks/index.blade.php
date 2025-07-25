9fd971ae417504165b8e6d2005d505ecca358f7b
rafi021 <sitc.mahmud@gmail.com>
2025-07-25 20:35:41 +0600

    task index page design

--- resources/views/pages/tasks/index.blade.php
+++ resources/views/pages/tasks/index.blade.php
@@ -1,4 +1,49 @@
 @extends('welcome')
 @section('main-content')
-    <h1>Task Index</h1>
+    <div class="container mx-auto space-y-12 space-x-5">
+        <div class="-m-1.5 overflow-x-auto pt-50">
+            <h1 class="text-2xl font-bold pt-12">Task List</h1>
+
+            <div class="flex justify-end mb-4">
+                <a href="" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">Task Create</a>
+            </div>
+
+            <div class="p-1.5 min-w-full inline-block align-middle border rounded-lg">
+                <div class="overflow-hidden">
+                    <table class="min-w-full table-auto divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                    #</th>
+                                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                    Task Name</th>
+                                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                    Date</th>
+                                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                    Status</th>
+                                <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">
+                                    Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200">
+
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">1</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">Task Name</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">2025-12-12</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">Pending</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                    <button type="button"
+                                        class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Edit</button>
+                                    <button type="button"
+                                        class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-red-600 hover:text-red-800 focus:outline-hidden focus:text-red-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
