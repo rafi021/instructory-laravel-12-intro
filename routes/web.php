cc5ae59d6940b9e41c3b423b13f048e7ff51acd6
rafi021 <sitc.mahmud@gmail.com>
2025-08-21 20:25:35 +0600

    useing laravel ui

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,15 @@ use App\Http\Controllers\RegisterController;
 // Group
 
 
-Route::get('register', [RegisterController::class, 'create'])->name('register.form');
-Route::post('register', [RegisterController::class, 'store'])->name('register.store');
-Route::get('login', [LoginController::class, 'create'])->name('login');
-Route::post('login', [LoginController::class, 'store'])->name('login.store');
-Route::middleware(['auth'])->group(function () {
-    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
-    require __DIR__ . '/task.php';
-});
+// Route::get('register', [RegisterController::class, 'create'])->name('register.form');
+// Route::post('register', [RegisterController::class, 'store'])->name('register.store');
+// Route::get('login', [LoginController::class, 'create'])->name('login');
+// Route::post('login', [LoginController::class, 'store'])->name('login.store');
+// Route::middleware(['auth'])->group(function () {
+//     Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+//     require __DIR__ . '/task.php';
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
